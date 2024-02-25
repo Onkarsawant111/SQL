36f434d051e880174c5c2d7ffafe66a62941ff59
onkar <onkarsawant111@gmail.com>
2024-02-25 12:11:22 +0530

    updated commands

--- sql-inbuilt-functions-4.sql
+++ sql-inbuilt-functions-4.sql
@@ -36,12 +36,12 @@ VALUES
 (10408, 'Elroy', 'Cleaver', 'Globe', 'Arizona');
 select * from ITEMS_ORDERED;
 select * from customers;
--- 1.How many people are in each unique state in the customers table? Select the state and display the number of people in each. Hint: count is used to count rows in a column, sum works on numeric data only.
-
--- 2.From the items_ordered table, select the item, maximum price, and minimum price for each specific item in the table. Hint: The items will need to be broken up into separate groups.
-
+-- 1.How many people are in each unique state in the customers table? Select the state and display the number of people in each. 
+select state,count(customerid) as no_of_people from customers group by state;
+-- 2.From the items_ordered table, select the item, maximum price, and minimum price for each specific item in the table.
+select item, min(price), max(price) from items_ordered group by item;
 -- 3.How many orders did each customer make? Use the items_ordered table. Select the customerid, number of orders they made, and the sum of their orders. 
-
+select customerid, sum(quantity) as total_orders from items_ordered group by customerid;
 
 
 
